c1fd80d8a3973cd4481cae03f74ac3f9934956e1
tomozoushanxia9 <tomozoushanxia9@gmail.com>
2019-12-26 01:02:05 +0900

    added production.py and local.py and base.py

---
+++ portfolio3/settings/local.py
@@ -0,0 +1,7 @@
+from .base import *
+
+SECRET_KEY = '6(wsnthx&f6h95(-3kp85$6(xznlosbbn)^_r=(0i==*d60(bp'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ portfolio3/settings/production.py
@@ -0,0 +1,8 @@
+from .base import *
+
+SECRET_KEY = os.environ.get('6(wsnthx&f6h95(-3kp85$6(xznlosbbn)^_r=(0i==*d60(bp')
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['just-a-fan-of-fkk.com', '.just-a-fan-of-fkk.com']
